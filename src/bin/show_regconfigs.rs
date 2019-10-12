823b4bf74bfe0755de8d774e874bf1b3e9eef5ce
Stephen Karger <stephen.karger@gmail.com>
2019-10-12 16:56:04 -0400

    Create show_regconfigs executable

---
+++ src/bin/show_regconfigs.rs
@@ -0,0 +1,21 @@
+extern crate diesel_regconfig;
+extern crate diesel;
+
+use self::diesel_regconfig::*;
+use self::models::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use diesel_regconfig::schema::regconfigs::dsl::*;
+
+    let connection = establish_connection();
+    let results = regconfigs
+        .limit(5)
+        .load::<RegConfig>(&connection)
+        .expect("Error loading regconfigs");
+
+    println!("Displaying {} regconfigs", results.len());
+    for regconfig in results {
+        println!("{}", regconfig.id);
+    }
+}
